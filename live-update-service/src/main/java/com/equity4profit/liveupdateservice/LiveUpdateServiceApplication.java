ebab4b5b8573ac92afe7cc5bb2cb014dd46f31c8
Rushabh82 <makanirushabh0786@gmail.com>
2025-01-09 14:30:43 +0530

    test commit by sub-repo.

--- live-update-service/src/main/java/com/equity4profit/liveupdateservice/LiveUpdateServiceApplication.java
+++ live-update-service/src/main/java/com/equity4profit/liveupdateservice/LiveUpdateServiceApplication.java
@@ -1,12 +1,17 @@
 package com.equity4profit.liveupdateservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LiveUpdateServiceApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(LiveUpdateServiceApplication.class);
+
     public static void main(String[] args) {
+        LOGGER.info("APPLICATION-STARTING");
         SpringApplication.run(LiveUpdateServiceApplication.class, args);
     }
 
