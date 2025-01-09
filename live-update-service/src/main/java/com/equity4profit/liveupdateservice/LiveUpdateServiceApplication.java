27460da56ec057c67c1fff6b3779bdb351829d6a
Rushabh82 <makanirushabh0786@gmail.com>
2025-01-09 14:21:00 +0530

    add-live update service branch's code structure.

---
+++ live-update-service/src/main/java/com/equity4profit/liveupdateservice/LiveUpdateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.equity4profit.liveupdateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveUpdateServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveUpdateServiceApplication.class, args);
+    }
+
+}
