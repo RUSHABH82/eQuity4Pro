2fa9c6984b3feac6d3ab7b400d3d98953b63dc23
Rushabh82 <makanirushabh0786@gmail.com>
2025-01-13 17:29:44 +0530

    add remote config for data-service

---
+++ data-history-service/src/main/java/com/equity4profit/datahistoryservice/config/CompanyCategoryConfig.java
@@ -0,0 +1,22 @@
+package com.equity4profit.datahistoryservice.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+import java.util.Set;
+
+@Configuration
+@ConfigurationProperties(prefix = "equity4profit")
+public class CompanyCategoryConfig {
+
+    private Map<String, Set<String>> categories;
+
+    public Map<String, Set<String>> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Map<String, Set<String>> categories) {
+        this.categories = categories;
+    }
+}
