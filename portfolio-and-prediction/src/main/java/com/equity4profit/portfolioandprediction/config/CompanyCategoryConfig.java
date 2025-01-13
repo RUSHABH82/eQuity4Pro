94c04a0a34cdc88833c13f1f1cff9758606ea8e5
Rushabh82 <makanirushabh0786@gmail.com>
2025-01-13 16:58:36 +0530

    Add config class

---
+++ portfolio-and-prediction/src/main/java/com/equity4profit/portfolioandprediction/config/CompanyCategoryConfig.java
@@ -0,0 +1,23 @@
+package com.equity4profit.portfolioandprediction.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Configuration
+@ConfigurationProperties(prefix = "equity4profit")
+public class CompanyCategoryConfig {
+
+    private Map<String, Set<String>> categories;
+
+    public Map<String, Set<String>> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Map<String, Set<String>> categories) {
+        this.categories = categories;
+    }
+}
