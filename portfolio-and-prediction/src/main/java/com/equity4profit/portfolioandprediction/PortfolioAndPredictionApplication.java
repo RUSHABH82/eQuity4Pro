41ccf25ebee890c2c0db4b88b36fb7311810ed56
Rushabh82 <makanirushabh0786@gmail.com>
2025-01-13 13:31:25 +0530

    add initial code push

---
+++ portfolio-and-prediction/src/main/java/com/equity4profit/portfolioandprediction/PortfolioAndPredictionApplication.java
@@ -0,0 +1,13 @@
+package com.equity4profit.portfolioandprediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioAndPredictionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioAndPredictionApplication.class, args);
+    }
+
+}
---
+++ portfolio-and-prediction/src/test/java/com/equity4profit/portfolioandprediction/PortfolioAndPredictionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equity4profit.portfolioandprediction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioAndPredictionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
